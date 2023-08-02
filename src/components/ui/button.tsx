f198db6b725934906829fe8b5c0d21e13de1a4a9
Alban Thaumur <th.alban@outlook.fr>
2023-08-02 08:18:35 +0200

    :sparkles: Add a reusable button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,23 @@
+import styles from './button.module.css'
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary';
+    outlined?: boolean;
+}
+
+export default function Button({
+    children,
+    variant = 'primary',
+    outlined = false,
+    ...props
+}: ButtonProps) {
+    return (
+        <button
+            className={`${styles.button} ${styles[`button--${variant}`]} ${outlined ? styles['button--outline'] : ''}`}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
