1204c3545085c726ab8ef796548f9bc1dfbbf31c
Alban Thaumur <th.alban@outlook.fr>
2023-08-02 18:33:24 +0200

    :sparkles: Add an Introduction component in the Home page
    And encapsulate content into a container that does not take the whole page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/header/header";
 import styles from "./root.module.css";
+import Introduction from "@/components/introduction/introduction";
 
 export default function Home() {
   return (
     <main className={styles.home}>
-      <Header />
-      Main Page
+      <div className={styles.home__container}>
+        <Header />
+        <Introduction />
+      </div>
     </main>
   )
 }
