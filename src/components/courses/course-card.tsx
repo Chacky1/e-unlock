4774222ac941999d76e1c734242f9bb0bb5a81eb
Alban Thaumur <th.alban@outlook.fr>
2023-08-19 20:10:17 +0200

    :lipstick: Add styles and inactive variant to course cards

--- src/components/courses/course-card.tsx
+++ src/components/courses/course-card.tsx
@@ -1,22 +1,26 @@
 import Image from 'next/image';
 import Button from '@/components/ui/button';
 import styles from './course-card.module.css';
+import { Course } from '@/lib/api/learning/schema/course.schema';
 
 type SaleCourseCardProps = {
-    course: any;
+    course: Course;
+    inactive?: boolean;
 };
 
-const CourseCard = ({ course }: SaleCourseCardProps) => {
+const CourseCard = ({ course, inactive }: SaleCourseCardProps) => {
   return (
     <div className={styles.course}>
       <div className={styles.course__image}>
-        <Image src={course.image} alt={course.name} width={300} height={300} />
+        <Image src={course.image} alt={course.name} width={300} height={200} />
       </div>
       <div className={styles.course__content}>
         <h3>{course.issue}</h3>
         <p>{course.description}</p>
         <Button>{course.name}</Button>
       </div>
+
+      {inactive && <div className={styles.course__inactive}></div>}
     </div>
   );
 };
