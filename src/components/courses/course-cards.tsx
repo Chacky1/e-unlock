a50bcc4bb86810e9156a1a8bd94fcdc345519b90
Alban Thaumur <th.alban@outlook.fr>
2023-08-19 20:12:39 +0200

    :sparkles: Add a default course to be printed when no course exists

--- src/components/courses/course-cards.tsx
+++ src/components/courses/course-cards.tsx
@@ -1,13 +1,27 @@
 import fetchCoursesByCategory from "@/actions/get-courses-by-category";
 import styles from "./course-cards.module.css";
 import CourseCard from "./course-card";
+import { Course } from "@/lib/api/learning/schema/course.schema";
 
 type CourseCardsProps = {
   categoryId: number;
 };
 
 const CourseCards = async ({ categoryId }: CourseCardsProps) => {
-  let courses = [];
+  let courses = [] as Course[];
+
+  const defaultCourse = {
+    name: "En construction",
+    issue: "Je veux me former tout de suite",
+    description:
+      "Malheureusement, je travaille encore sur cette future formation...",
+    price: 0,
+    image: "/default-course.webp",
+    categoryId: 0,
+    id: 0,
+    createdAt: new Date().toString(),
+    updatedAt: new Date().toString(),
+  };
 
   try {
     courses = await fetchCoursesByCategory(categoryId);
@@ -17,20 +31,19 @@ const CourseCards = async ({ categoryId }: CourseCardsProps) => {
 
   return (
     <section id="courses" className={styles.courses}>
-      {courses.length > 0 && (
-        <ul className={styles.courses__list}>
-          {courses.map((course: any) => (
+      <ul className={styles.courses__list}>
+        {courses.length > 0 &&
+          courses.map((course: any) => (
             <li key={course.id}>
               <CourseCard course={course} />
             </li>
           ))}
-        </ul>
-      )}
-      {courses.length === 0 && (
-        <p className={styles.courses__construction}>
-          En cours de construction...
-        </p>
-      )}
+        {courses.length === 0 && (
+          <li>
+            <CourseCard course={defaultCourse} inactive />
+          </li>
+        )}
+      </ul>
     </section>
   );
 };
