68389e621ece84123bde1228ae42f050d787ec25
Alban Thaumur <th.alban@outlook.fr>
2023-08-29 17:42:47 +0200

    :label: Add types in fetch actions

--- src/actions/fetch-categories.ts
+++ src/actions/fetch-categories.ts
@@ -1,6 +1,7 @@
 import { clientApiLearning } from "@/lib/api/learning/client";
+import { Category } from "@/lib/api/learning/schema/category.schema";
 
-const fetchCategories = async () => {
+const fetchCategories = async (): Promise<Category[]> => {
   const categories = await clientApiLearning.fetchCategories();
 
   return categories;
--- src/actions/fetch-courses-by-category.ts
+++ src/actions/fetch-courses-by-category.ts
@@ -1,6 +1,7 @@
 import { clientApiLearning } from "@/lib/api/learning/client";
+import { Course } from "@/lib/api/learning/schema/course.schema";
 
-const fetchCoursesByCategory = async (categoryId: number) => {
+const fetchCoursesByCategory = async (categoryId: number): Promise<Course[]> => {
   const courses = await clientApiLearning.fetchCoursesByCategory(categoryId);
 
   return courses;
