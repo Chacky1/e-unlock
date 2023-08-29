2b6cba608871155a3ea8a0f2a7b0203cc6de3e7b
Alban Thaumur <th.alban@outlook.fr>
2023-08-29 17:44:36 +0200

    :label: Update course schema with slug, solution and video

--- src/lib/api/learning/schema/course.schema.ts
+++ src/lib/api/learning/schema/course.schema.ts
@@ -3,11 +3,14 @@ import { z } from "zod";
 export const CourseSchema = z.object({
     id: z.number(),
     name: z.string().min(1).max(255),
+    slug: z.string().min(1).max(255),
     description: z.string(),
     issue: z.string(),
+    solution: z.string(),
     price: z.number(),
     categoryId: z.number(),
     image: z.string().url(),
+    video: z.string().url(),
     createdAt: z.string().datetime(),
     updatedAt: z.string().datetime(),
 });
