9abcdeb29bd4880710606ebe5d3fc62c0ead740f
Alban Thaumur <th.alban@outlook.fr>
2023-10-18 15:22:37 +0200

    :sparkles: Add a Link to allow user to be redirected when clicking logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -22,14 +22,16 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <section id="logo" className={styles.logo}>
-        <Image
-          src="/unlock-logo.webp"
-          width={40}
-          height={40}
-          alt="UnlockTonComputer"
-        />
-        <h1 className={styles.title}>e-unlock</h1>
+      <section id="logo">
+        <Link href="/" className={styles.logo}>
+          <Image
+            src="/unlock-logo.webp"
+            width={40}
+            height={40}
+            alt="UnlockTonComputer"
+          />
+          <h1 className={styles.title}>e-unlock</h1>
+        </Link>
       </section>
 
       <section id="navigation" className={styles.auth}>
@@ -38,16 +40,10 @@ export default function Header() {
             <nav className={styles.navigation}>
               <ul>
                 <li className={styles.navigation__link}>
-                  <Link href="/">
-                    Accueil
-                  </Link>
+                  <Link href="/">Accueil</Link>
                 </li>
                 <li className={styles.navigation__link}>
-                  <Link
-                    href={`/profile/${userId}`}
-                  >
-                    Mes cours
-                  </Link>
+                  <Link href={`/profile/${userId}`}>Mes cours</Link>
                 </li>
               </ul>
             </nav>
