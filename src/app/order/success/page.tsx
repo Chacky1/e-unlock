5b220289a69824320d207cd047c6af90b5385873
Alban <th.alban@outlook.fr>
2023-11-04 15:37:28 +0100

    :lipstick: Add some styles on success sale pages

--- src/app/order/success/page.tsx
+++ src/app/order/success/page.tsx
@@ -1,22 +1,30 @@
 import createOrder from "@/actions/create-order";
 import Spinner from "@/components/ui/spinner";
 
-type orderSuccessPageProps = {
+import styles from "./page.module.css";
+
+type OrderSuccessPageProps = {
   searchParams: {
     courseId: string;
     userCode: string;
   };
 };
 
-const OrderSuccessPage = async ({ searchParams }: orderSuccessPageProps) => {
+const OrderSuccessPage = async ({ searchParams }: OrderSuccessPageProps) => {
   const { courseId, userCode } = searchParams;
 
-  await createOrder(userCode, +courseId, "SUCCESS");
+  try {
+    console.log(`Creating a SUCCESS order for user ${userCode} and course ${courseId}...`);
+    await createOrder(userCode, +courseId, "SUCCESS");
+  }
+  catch (error) {
+    console.log(`Error while creating order : ${error}`);
+  }
 
   return (
-    <main>
-      <h1>Votre achat a été effectué avec succès 🥳</h1>
-      <p>Vous allez être redirigé vers votre dashboard...</p>
+    <main className={styles['order-success']}>
+      <h1 className={styles['order-success__title']}>Votre achat a été effectué avec succès 🥳</h1>
+      <p className={styles['order-success__text']}>Vous allez être redirigé vers votre dashboard...</p>
       <Spinner redirectPath={`/profile/${userCode}`} />
     </main>
   );
