c6ba45699ac1c72896bf364edb9ec6376b629ae1
Alban <th.alban@outlook.fr>
2023-11-04 15:37:03 +0100

    :lipstick: Add some styles and content on cancelled sale pages

--- src/app/order/cancel/page.tsx
+++ src/app/order/cancel/page.tsx
@@ -1,8 +1,33 @@
-const OrderCancelledPage = () => {
+import createOrder from '@/actions/create-order';
+import Spinner from '@/components/ui/spinner';
+
+import styles from './page.module.css';
+
+type OrderCancelledPageProps = {
+  searchParams: {
+    courseId: string;
+    userCode: string;
+  };
+};
+
+const OrderCancelledPage = async ({ searchParams }: OrderCancelledPageProps) => {
+  const { courseId, userCode } = searchParams;
+  
+  try {
+    console.log(`Creating a CANCELLED order for user ${userCode} and course ${courseId}...`);
+    await createOrder(userCode, +courseId, "CANCELLED");
+  }
+  catch (error) {
+    console.log(`Error while creating order : ${error}`);
+  }
+
   return (
-    <div>
-      <h1>Order Cancelled</h1>
-      <p>Your order has been cancelled.</p>
-    </div>
+    <main className={styles['order-cancelled']}>
+      <h1 className={styles['order-cancelled__title']}>Vous n'avez pas terminé votre achat... 😢</h1>
+      <p className={styles['order-cancelled__text']}>Vous allez être redirigé vers la page d'accueil...</p>
+      <Spinner redirectPath="/" />
+    </main>
   );
 };
+
+export default OrderCancelledPage;
