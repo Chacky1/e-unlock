603cbf7ab84c71f197854a81a23ba4e5de5f3fd3
Alban Thaumur <th.alban@outlook.fr>
2023-11-10 13:33:47 +0100

    :sparkles: Add the possibility to submit a form with UI button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,12 +4,14 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     variant?: 'primary' | 'secondary' | 'tertiary';
     outlined?: boolean;
+    type?: 'button' | 'submit' | 'reset';
 }
 
 export default function Button({
     children,
     variant = 'primary',
     outlined = false,
+    type = 'button',
     ...props
 }: ButtonProps) {
     return (
