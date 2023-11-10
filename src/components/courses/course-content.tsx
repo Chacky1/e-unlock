dde5cb2fbb5017000a55e2f217371ec14c1cb9a5
Alban Thaumur <th.alban@outlook.fr>
2023-11-10 18:45:26 +0100

    :lipstick: Add lesson resources and title to lesson pages

--- src/components/courses/course-content.tsx
+++ src/components/courses/course-content.tsx
@@ -2,6 +2,7 @@ import { toast } from "react-toastify";
 import fetchLessonById from "@/actions/fetch-lesson-by-id";
 
 import styles from "./course-content.module.css";
+import fetchResourcesByLesson from "@/actions/fetch-resources-by-lesson";
 
 type CourseContentProps = {
   lessonId: number;
@@ -9,6 +10,7 @@ type CourseContentProps = {
 
 const CourseContent = async ({ lessonId }: CourseContentProps) => {
   const lesson = await fetchLessonById(lessonId);
+  const lessonResource = await fetchResourcesByLesson(lessonId);
 
   if (!lesson) {
     return toast.error("🚨 La leçon ne s'est pas chargée correctement", {
@@ -25,7 +27,26 @@ const CourseContent = async ({ lessonId }: CourseContentProps) => {
 
   return (
     <div className={styles.lesson}>
-      <video src={lesson.videoUrl} controls className={styles["lesson__video"]}/>
+      <video
+        src={lesson.videoUrl}
+        controls
+        className={styles["lesson__video"]}
+      />
+      <h1 className={styles["lesson__title"]}>{lesson.name}</h1>
+      <ul className={styles["lesson__resources"]}>
+        {lessonResource.map((resource) => (
+          <li key={resource.id} className={styles["lesson__resource"]}>
+            <a
+              href={resource.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              download={resource.name}
+            >
+              {resource.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
