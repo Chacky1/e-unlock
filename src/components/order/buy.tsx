3cb6f2d406d984ac539fd1f17b22d8de5488514a
Alban Thaumur <th.alban@outlook.fr>
2023-11-11 18:46:53 +0100

    :lipstick: Change buy button content when user already has a course

--- src/components/order/buy.tsx
+++ src/components/order/buy.tsx
@@ -1,26 +1,111 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { useAuth } from "@clerk/nextjs";
 import { loadStripe } from "@stripe/stripe-js";
 
 import Button from "@/components/ui/button";
+import { OrderStatus } from "@/lib/api/learning/schema/order.schema";
 
 interface BuyProps {
   priceId: string;
   nextStripePublicKey: string;
   courseId: number;
+  hasUserBoughtCourse: boolean;
 }
 
-const Buy = ({ priceId, nextStripePublicKey, courseId }: BuyProps) => {
+const Buy = ({
+  priceId,
+  nextStripePublicKey,
+  courseId,
+  hasUserBoughtCourse,
+}: BuyProps) => {
   const router = useRouter();
   const { userId } = useAuth();
 
-  const onBuyClick = async () => {
-    const stripe = await loadStripe(nextStripePublicKey);
+  const onSignInClick = () => {
+    const currentPath = window.location.pathname;
+    router.push(`/sign-in?redirect=${currentPath}`);
+  };
 
-    if (!stripe) {
+  if (!userId)
+    return (
+      <Button
+        variant="secondary"
+        style={{ color: "#fff" }}
+        onClick={onSignInClick}
+      >
+        Acheter
+      </Button>
+    );
+
+  const redirectToDashboard = () => {
+    router.push(`/profile/${userId}`);
+  };
+
+  const createOrderOrFail = async (
+    userId: string,
+    courseId: number,
+    status: OrderStatus
+  ) => {
+    try {
+      const order = await fetch("/order/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userCode: userId,
+          courseId,
+          status,
+        }),
+      });
+
+      return order.json();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const buyCourse = async () => {
+    try {
+      console.log(
+        `Creating a PENDING order for user ${userId} and course ${courseId}...`
+      );
+      const order = await createOrderOrFail(
+        userId,
+        courseId,
+        OrderStatus.PENDING
+      );
+
+      const stripe = await loadStripe(nextStripePublicKey);
+
+      if (!stripe) {
+        return toast.error("🚨 Une erreur est survenue", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+
+      const { error } = await stripe.redirectToCheckout({
+        lineItems: [{ price: priceId, quantity: 1 }],
+        mode: "payment",
+        successUrl: `${window.location.origin}/order/success?orderId=${order.id}&userCode=${userId}`,
+        cancelUrl: `${window.location.origin}/order/cancel?orderId=${order.id}`,
+      });
+
+      if (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.error(error);
       return toast.error("🚨 Une erreur est survenue", {
         position: "top-right",
         autoClose: 5000,
@@ -32,32 +117,24 @@ const Buy = ({ priceId, nextStripePublicKey, courseId }: BuyProps) => {
         theme: "light",
       });
     }
-
-    const { error } = await stripe.redirectToCheckout({
-      lineItems: [{ price: priceId, quantity: 1 }],
-      mode: "payment",
-      successUrl: `${window.location.origin}/order/success?courseId=${courseId}&userCode=${userId}`,
-      cancelUrl: `${window.location.origin}/order/cancel`,
-    });
-
-    if (error) {
-      console.log(error);
-    }
-  };
-
-  const onSignInClick = () => {
-    const currentPath = window.location.pathname;
-    router.push(`/sign-in?redirect=${currentPath}`);
   };
 
   return (
     <>
-      {userId ? (
-        <Button variant="secondary" style={{color: "#fff"}} onClick={onBuyClick}>
-          Acheter
+      {hasUserBoughtCourse ? (
+        <Button
+          variant="secondary"
+          style={{ color: "#fff" }}
+          onClick={redirectToDashboard}
+        >
+          Suivre le cours
         </Button>
       ) : (
-        <Button variant="secondary" style={{color: "#fff"}} onClick={onSignInClick}>
+        <Button
+          variant="secondary"
+          style={{ color: "#fff" }}
+          onClick={buyCourse}
+        >
           Acheter
         </Button>
       )}
