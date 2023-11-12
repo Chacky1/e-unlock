cd797bbd85a374e01c741a84752a8d3460367b79
Alban Thaumur <th.alban@outlook.fr>
2023-11-12 15:12:03 +0100

    :mute: Remove logs in learning api client order creation process

--- src/lib/api/learning/client.ts
+++ src/lib/api/learning/client.ts
@@ -207,9 +207,7 @@ class ClientApiLearning {
     courseId: number,
     status: string
   ): Promise<Order> => {
-    console.log("[createOrderOrFail] parameters", userCode, courseId, status);
     await this.ensureAccessToken();
-    console.log("[createOrderOrFail] accessToken : ", this.accessToken);
     const ordersUrl = new URL("orders", AUTH0_AUDIENCE);
 
     const response = await fetch(ordersUrl, {
